aee80bb7105fb04093975757475352b44f650332
Vilma Kankaanpää <kankaanpaavilma@gmail.com>
2019-03-13 09:37:29 +0200

    settings.py typo fixed

--- bubbleproject/bubbleproject/settings.py
+++ bubbleproject/bubbleproject/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bubbleapp.apps.DjangoappConfig',
+    'bubbleapp.apps.BubbleappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
