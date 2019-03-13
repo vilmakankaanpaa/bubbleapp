e8b0042b321e8b2e2712e102b439b3ad21e3d1f1
Vilma Kankaanpää <kankaanpaavilma@gmail.com>
2019-03-13 09:30:16 +0200

    settings.py add bubbleapp to installed apps

--- bubbleproject/bubbleproject/settings.py
+++ bubbleproject/bubbleproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bubbleapp.apps.DjangoappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
