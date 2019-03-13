385d8086bab3e06ed43c6988921395b2bd87f19f
Vilma Kankaanpää <kankaanpaavilma@gmail.com>
2019-03-13 15:11:00 +0200

    using django.contrib.auth loginview

--- bubbleproject/bubbleapp/urls.py
+++ bubbleproject/bubbleapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import LoginView
 
+app_name = 'bubbleapp'
 urlpatterns = [
   path('', views.index, name='index'),
-  path('login/', views.login, name='login'),
+  path('login/', LoginView.as_view(template_name='bubbleapp/login.html')),
 ]
