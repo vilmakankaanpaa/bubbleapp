3a744e7e236a99f3f85be4690283d37149ac8dd1
Vilma Kankaanpää <kankaanpaavilma@gmail.com>
2019-03-13 17:53:19 +0200

    create logout and registrations views

--- bubbleproject/bubbleapp/urls.py
+++ bubbleproject/bubbleapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'bubbleapp'
 urlpatterns = [
   path('', views.index, name='index'),
   path('login/', LoginView.as_view(template_name='bubbleapp/login.html')),
+  path('logout/', LogoutView.as_view(template_name='bubbleapp/logout.html')),
+  path('register/', views.register, name='register'),
 ]
--- bubbleproject/bubbleapp/views.py
+++ bubbleproject/bubbleapp/views.py
@@ -1,10 +1,19 @@
-import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'bubbleapp/index.html', {})
 
-def login(request):
-    return render(request, 'bubbleapp/login.html', {})
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/bubbleapp')
+    else:
+        form = UserCreationForm()
+
+        args = {'form': form}
+        return render(request, 'bubbleapp/req_from.html', args)
