3441361bad9205bb16efbdbdc6c506bf7c39e8ab
Vilma Kankaanpää <kankaanpaavilma@gmail.com>
2019-03-13 14:16:06 +0200

    starting to build login page

--- bubbleproject/bubbleapp/urls.py
+++ bubbleproject/bubbleapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'djangoapp'
 urlpatterns = [
   path('', views.index, name='index'),
+  path('login/', views.login, name='login'),
 ]
--- bubbleproject/bubbleapp/views.py
+++ bubbleproject/bubbleapp/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return render(request, 'bubbleapp/index.html', {})
+    return render(request, 'bubbleapp/index.html', {})
+
+def login(request):
+    return render(request, 'bubbleapp/login.html', {})
