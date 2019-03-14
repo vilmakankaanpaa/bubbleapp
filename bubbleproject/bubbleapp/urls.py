639dd5a36f5312ea530060a92bb98fd46a8c8e40
Vilma Kankaanpää <kankaanpaavilma@gmail.com>
2019-03-14 12:50:49 +0200

    new views

--- bubbleproject/bubbleapp/urls.py
+++ bubbleproject/bubbleapp/urls.py
@@ -5,7 +5,10 @@ from django.contrib.auth.views import LoginView, LogoutView
 app_name = 'bubbleapp'
 urlpatterns = [
   path('', views.index, name='index'),
+  path('<int:hashtag_id>/', views.detail, name='detail'),
+  path('feed/', views.feed, name='feed'),
   path('login/', LoginView.as_view(template_name='bubbleapp/login.html')),
   path('logout/', LogoutView.as_view(template_name='bubbleapp/logout.html')),
+  path('profile/', views.profile, name='profile'),
   path('register/', views.register, name='register'),
 ]
--- bubbleproject/bubbleapp/views.py
+++ bubbleproject/bubbleapp/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.template import loader
+
+from .models import Hashtag, Account
 
 # Create your views here.
 
+def detail(request, hashtag_id):
+    hashtag = get_object_or_404(Hashtag, pk=hashtag_id)
+    return render(request, 'bubbleapp/detail.html', {'hashtag': hashtag})
+
+def feed(request):
+    my_hashtags = Hashtag.objects.order_by('-add_date')
+    args = { 'my_hashtags': my_hashtags}
+    return render(request, 'bubbleapp/feed.html', args)
+
 def index(request):
     return render(request, 'bubbleapp/index.html', {})
+
+def profile(request):
+    return render(request, 'bubbleapp/profile.html', {})
 
 def register(request):
     if request.method == 'POST':
