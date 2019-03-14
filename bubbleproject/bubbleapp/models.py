4971fced971a9b77456a46163e5a0c469eea6644
Vilma Kankaanpää <kankaanpaavilma@gmail.com>
2019-03-14 09:59:47 +0200

    new models: hashtag and account

--- bubbleproject/bubbleapp/models.py
+++ bubbleproject/bubbleapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=200)
+    hashtag = models.CharField(max_length=100)
+    add_date = models.DateTimeField('date added')
+
+    def __str__(self):
+        return self.name
+
+class Account(models.Model):
+    name = models.CharField(max_length=200)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
