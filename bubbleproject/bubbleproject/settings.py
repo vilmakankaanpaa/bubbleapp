1dbb6c8a86aa686ec5597e86404f8e9d4125c583
Vilma Kankaanpää <kankaanpaavilma@gmail.com>
2019-03-18 22:24:33 +0200

    read API_key from env variables

--- bubbleproject/bubbleproject/settings.py
+++ bubbleproject/bubbleproject/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
+BDB_API_KEY = os.environ['BDB_API_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
