ac03eae5d81c435d45082a940b18cbacc69ffa45
Vilma Kankaanpää <kankaanpaavilma@gmail.com>
2019-03-18 22:21:18 +0200

    breweryDb models updated

--- bubbleproject/bubbleapp/models.py
+++ bubbleproject/bubbleapp/models.py
@@ -20,21 +20,28 @@ class Account(models.Model):
 class Beer(models.Model):
     id = models.CharField(primary_key=True, max_length=50)
     name = models.CharField('beer name', max_length=200)
-    abv = models.FloatField('Alcohol by volume')
-    style = models.ForeignKey('Style', on_delete=models.PROTECT)
-    isOrganic = models.BooleanField()
-    isRetired = models.BooleanField()
-    createDtae = models.DateField('Created')
-    updateDate = models.DateField('Updated')
-    glassName = models.CharField('Glass', max_length = 200)
-    styleDescription = models.TextField('Description')
+    abv = models.FloatField('Alcohol by volume', default=None, null=True, blank=True)
+    style = models.ForeignKey('Style', on_delete=models.PROTECT, blank=True, null=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, blank=True, null=True)
+    isOrganic = models.BooleanField(blank=True, null=True)
+    isRetired = models.BooleanField(blank=True, null=True)
+    createDate = models.DateTimeField('Created', blank=True, null=True)
+    updateDate = models.DateTimeField('Updated', blank=True, null=True)
+    glassName = models.CharField('Glass', max_length = 200, blank=True)
+    styleDescription = models.TextField('Description', blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Style(models.Model):
-    id = models.CharField(primary_key=True)
-    categoryName = models.ForeignKey('Category', on_delete=models.PROTECT)
-    styleName = models.CharField(max_length=200)
-    shortname =models.CharField(max_length=200)
+    styleName = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.styleName
 
 class Category(models.Model):
-    id = models.CharField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
